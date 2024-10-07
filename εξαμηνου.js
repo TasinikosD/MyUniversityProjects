abdb209ea4766bc21d49a1d489c5bbf44bca750c
TasinikosD <167218361+TasinikosD@users.noreply.github.com>
2024-10-07 16:39:48 +0300

    Delete εξαμηνου.js

--- εξαμηνου.js
+++
@@ -1,9 +0,0 @@
-/* δημιουργία συνάρτησης για εμφάνιση και μη του κωδικού στα πεδία register και login */
-function myFunction() {
-  var x = document.getElementById("password");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
